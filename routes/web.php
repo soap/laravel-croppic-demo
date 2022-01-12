fb49f83889e680096923cf04dc217918585afa4a
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2022-01-12 13:55:11 +0700

    Update to Laravel 8.x, install laravel/breeze starter kit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +16,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
